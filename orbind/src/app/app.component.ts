f3a07ec80497c2430c9fafb4b8dde2233f5e79f4
Marcin Binek <marcin.binek@summer.agency>
2017-06-13 13:14:11 +0200

    feature/binding

--- orbind/src/app/app.component.ts
+++ orbind/src/app/app.component.ts
@@ -6,5 +6,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app';
-}
+  title = 'orbind';
+
+searchValue:string = '';
+
+getMeData(){
+  let currentDate = new Date();
+  var data = currentDate.getFullYear();
+   var strTime = data - 2017;
+    return strTime + ' years old app';
+} 
+
+getMyCopy(){
+ console.log('copy');
+ 
+  }
+
+  clearSearch() {
+   window.location.reload();
+  }
+    
+};
+
+
